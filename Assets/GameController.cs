758f1523c46e591f4a12289d27b1ab9ea6106b54
riwmd <kubilayege95@gmail.com>
2019-06-24 13:54:14 +0300

    AItargeting

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+    public GameObject[] bots;
+    public GameObject player;
+
     [SerializeField]
     GameObject playerPrefab;
-    [SerializeField]
-    GameObject[] bots;
     [SerializeField]
     GameObject[] carModels;
 
     [SerializeField]
     int numberOfBots;
     [SerializeField]
-    int chosenCar;
+    int chosenCar=0;
 
     void Start()
     {
@@ -32,7 +34,7 @@ public class GameController : MonoBehaviour
     {
 
         //player spawn
-        GameObject player = spawnObj(playerPrefab, getRandPos(), Quaternion.identity);
+        player = spawnObj(playerPrefab, getRandPos(1), Quaternion.identity);
         GameObject playerCar = spawnObj(carModels[chosenCar], player.transform.position , Quaternion.identity);
 
         playerCar.transform.parent = player.transform;
@@ -41,21 +43,22 @@ public class GameController : MonoBehaviour
         //bots spawn
         for (int i = 0; i < numberOfBots; i++)
         {
-            bots[i] = spawnObj(bots[i], getRandPos(), Quaternion.identity);
+            bots[i] = spawnObj(bots[i], getRandPos(i+1), Quaternion.identity);
             GameObject botCar = spawnObj(carModels[(int)UnityEngine.Random.Range(0,8)], bots[i].transform.position, Quaternion.identity);
             
 
             botCar.transform.parent = bots[i].transform;
-            bots[i].transform.forward = getRandPos();
+            bots[i].transform.forward = getRandPos(i+1);
+            bots[i].name = "Bot" + (i+1).ToString();
         }
     }
 
-    Vector3 getRandPos()
+    Vector3 getRandPos(int i)
     {
         UnityEngine.Random.seed = DateTime.UtcNow.Millisecond;
-        Vector3 randomPlace = new Vector3(UnityEngine.Random.Range(-38, 38),
+        Vector3 randomPlace = new Vector3(UnityEngine.Random.Range(-10.0f - i*4, 10.0f + i*4),
                                           1f,
-                                          UnityEngine.Random.Range(-38, 38)); 
+                                          UnityEngine.Random.Range(-10.0f - i*4, 10.0f + i*4)); 
 
         return randomPlace;
     }
---
+++ Assets/botCar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botCar : MonoBehaviour
+{
+    GameController gc;
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameController gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    
+    void Move()
+    {
+        int id = GetBotID(this.name);
+        target = FindClosest(id);
+    }
+    GameObject FindClosest(int thisID)
+    {
+        var maxdistance = 0.0f;
+        var distance = 0.0f;
+
+        GameObject closest= gc.player;
+
+        for (int i = 0; i < gc.bots.Length; i++)
+        {
+            if(i == thisID) { continue; }
+            distance = (gc.bots[i].transform.position - gc.bots[thisID].transform.position).magnitude; 
+            if(distance> maxdistance) { maxdistance = distance; closest = gc.bots[i].gameObject; }
+        }
+
+        distance = (gc.player.transform.position - gc.bots[thisID].transform.position).magnitude;
+        if(distance > maxdistance) { return gc.player; }
+
+        return closest;
+    }
+    int GetBotID(string name)
+    {
+        for(int i = 0; i < gc.bots.Length; i++)
+        {
+            if(gc.bots[i].name == name) return i;
+        }
+        return -1;
+    }
+}
