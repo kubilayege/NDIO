2252bcd38c1f83e4f8f3989580d2af3722e37195
kubilayege <kubilayege95@gmail.com>
2019-07-02 19:45:21 +0300

    gitpushEmre
    Bot AI fix
    Shield

--- Assets/GameScripts/PlayerController.cs
+++ Assets/GameScripts/PlayerController.cs
@@ -12,36 +12,33 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     [SerializeField]
     Camera cam;
+    GameObject scaler;
+    GameObject scaler2;
+    float knockbackcoff = 1000.0f;
+    int score;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        
+        scaler = GameObject.Find("Player(Clone)/Shield(Clone)");
+        scaler2 = GameObject.Find("Player(Clone)/Shield2(Clone)");
     }
     
     void Update()
     {
         MoveForward();
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && transform.position.y < 0.2)
         {
             Move();
         }
-        if (Input.GetMouseButtonDown(1))
-        {
-            KnockBack();
-        }
     }
 
-    void KnockBack()
-    {
-        rb.velocity = Vector3.zero;
-        rb.AddForce(-transform.forward * 1000.0f);
-    }
     void MoveForward()
     {
         rb.MovePosition(transform.position+(transform.forward * carSpeed * Time.deltaTime));
         cam.transform.position =  new Vector3(transform.position.x, transform.position.y + 10.0f, transform.position.z - 7.0f);
+      
     }
 
     void Move()
@@ -49,8 +46,9 @@ public class PlayerController : MonoBehaviour
         RaycastHit hit;
         Ray camDir = cam.ScreenPointToRay(Input.mousePosition);
         Physics.Raycast(camDir, out hit);
+        
         direction = (hit.point - transform.position).normalized * carSpeed * Time.deltaTime;
-
+        
         //transform.forward += direction;
         transform.forward += new Vector3(Mathf.Lerp(transform.position.x, direction.x, 1.2f),
                                         0,
@@ -60,4 +58,32 @@ public class PlayerController : MonoBehaviour
 
         //transform.rotation.SetLookRotation(direction.normalized, Vector3.up);
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Shield")
+        {
+            KnockBack();
+        }
+
+        if (other.gameObject.tag == "Car")
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+            KillSomeone();
+        }
+    }
+
+    void KnockBack()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(-transform.forward * knockbackcoff);
+    }
+
+    void KillSomeone()
+    {
+        score += 1;
+        scaler.gameObject.transform.localScale += new Vector3(0.5f, 0, 0);
+        scaler2.gameObject.transform.localScale += new Vector3(0.5f, 0, 0);
+
+    }
+
 }
--- Assets/botCar.cs
+++ Assets/botCar.cs
@@ -1,62 +1,97 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class botCar : MonoBehaviour
 {
     GameController gc;
     GameObject target;
     Rigidbody rb;
-    // Start is called before the first frame update
+    GameObject scaler;
+    GameObject scaler2; 
+    Vector3 direction; //yön bilgisi
+
+    public int score;
+    public float knockbackcoff = 1000.0f;
+
     void Start()
     {
         gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         rb = GetComponent<Rigidbody>();
+
+        scaler = GameObject.Find("Bot1/Shield(Clone)");
+        scaler2 = GameObject.Find("Bot1/Shield2(Clone)");
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
 
-    
+
     void Move()
     {
         int id = GetBotID(this.name);
         target = FindClosest(id);
+        try { direction = (target.transform.position - transform.position).normalized * 7 * Time.deltaTime; } catch { }
 
-        rb.MovePosition(transform.position + ( ( target.transform.position - transform.position).normalized * 10.0f * Time.deltaTime) );
-
-        transform.forward += new Vector3(Mathf.Lerp(transform.position.x, target.transform.position.x, 1.2f),
+        transform.forward += new Vector3(Mathf.Lerp(transform.position.x, direction.x, 1.2f),
                                         0,
-                                        Mathf.Lerp(transform.position.z, target.transform.position.z, 1.2f));
+                                        Mathf.Lerp(transform.position.z, direction.z, 1.2f));
+
+        rb.MovePosition(transform.position + direction);
+
     }
     GameObject FindClosest(int thisID)
     {
         var maxdistance = 0.0f;
         var distance = 0.0f;
 
-        GameObject closest= gc.player;
+        GameObject closest = gc.player;
 
         for (int i = 0; i < gc.bots.Length; i++)
         {
-            if(i == thisID) { continue; }
-            distance = (gc.bots[i].transform.position - gc.bots[thisID].transform.position).magnitude; 
-            if(distance> maxdistance) { maxdistance = distance; closest = gc.bots[i].gameObject; }
+            if (i == thisID) { continue; }
+            try { distance = (gc.bots[i].transform.position - gc.bots[thisID].transform.position).magnitude; } catch { }
+            if (distance > maxdistance) { maxdistance = distance; closest = gc.bots[i].gameObject; }
         }
 
-        distance = (gc.player.transform.position - gc.bots[thisID].transform.position).magnitude;
-        if(distance > maxdistance) { return gc.player; }
+        try { distance = (gc.player.transform.position - gc.bots[thisID].transform.position).magnitude; } catch { }
+        if (distance > maxdistance) { return gc.player; }
 
         return closest;
     }
     int GetBotID(string name)
     {
-        for(int i = 0; i < gc.bots.Length; i++)
+        for (int i = 0; i < gc.bots.Length; i++)
         {
-            if(gc.bots[i].name == name) return i;
+            try { if (gc.bots[i].name == name) return i; } catch { }
         }
         return -1;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Shield")
+        {
+            KnockBack();
+        }
+
+        if (other.gameObject.tag == "Car")
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+            KillSomeone();
+        }
+
+
+    }
+
+    void KnockBack()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(-transform.forward * knockbackcoff);
+    }
+    void KillSomeone()
+    {
+        score += 1;
+        scaler.gameObject.transform.localScale += new Vector3(0.5f, 0, 0);
+        scaler2.gameObject.transform.localScale += new Vector3(0.5f, 0, 0);
     }
 }
